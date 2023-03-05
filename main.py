c921abbbee3c248a0f3cb2e9e84ceaedd2692888
Michelle Lamelza <mishlamelza@gmail.com>
2023-03-05 16:36:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import bonus_aasignment as task_1
+import bonus_aasignment2 as task_2
+import bonus_assignment3 as task_3
+import bonus_assignment4 as task_4
+import bonus_assignment5 as task_5
+import bonus_assignment6 as task_6
+
