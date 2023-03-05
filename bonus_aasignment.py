91e6f7d1c9c55726c459eec089dbef81fe44553b
Michelle Lamelza <mishlamelza@gmail.com>
2023-03-05 06:03:10 -0500

    continued work on tasks
    #worked on tasks

--- bonus_aasignment.py
+++ bonus_aasignment.py
@@ -14,7 +14,28 @@ def first_and_last():
     print (first_letter + last_letter)
 # we call the function and therefore concatenates both together
 first_and_last
+
 #output is Ps
+
+# TASK 2: Peanut Butter & Jelly
+#created a function to print 0 to 100
+
+def numbers_in_range():
+    for i in range(0,101,1):
+        print(i) # output will print 0 to 100
+#call function
+numbers_in_range
+
+
+
+
+
+
+
+
+
+
+
 
 
 
